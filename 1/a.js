c28315a38ed1dd91732dfc95d6c3a900a31bc17f
Sofia Bargues <barguessofia@gmail.com>
2022-12-04 10:53:53 -0300

    Leer de archivo

--- 1/a.js
+++ 1/a.js
@@ -1,14 +1,35 @@
 /*
 
-Una consideración importante es la comida, en particular,
-la cantidad de calorías que lleva cada Elfo (su entrada de rompecabezas). 
-Los Elfos se turnan para anotar el número de Calorías que contienen las distintas comidas, 
-meriendas, raciones, etc. que han traído consigo, un elemento por línea. 
-Cada Elfo separa su propio inventario del inventario del Elfo anterior (si lo hay) con una línea en blanco. 
+Input:
+linea = cal de un ittem 
+lineas consecutivas = ittem de un elfo 
+salto de linea = elfo nuevo 
 
-input: kal por elfo
-cada linea corresponde a las kal de un elfo 
-linea en blanco para separar la comida anterior 
+Output:
+cal del elfo con mayor cal
 
 */
+
+
+const fs = require('fs');
+const lineReader = require('line-reader');
+
+function readData() {
+    let lines = []
+    try {
+        lineReader.eachLine('./1/a_input.txt', (line, last) => {
+            lines.push(line)
+            console.log(line);
+        });
+        // const content = fs.readFileSync('./1/a_input.txt', 'utf8');
+        // console.log(content);
+        return lines
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const data = readData();
+
+data
 
