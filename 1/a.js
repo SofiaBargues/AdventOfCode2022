4314f96ba0742ada0361dd0a2a830254f0303c22
Sofia Bargues <barguessofia@gmail.com>
2022-12-04 09:26:15 -0300

    Enunciado 1a

---
+++ 1/a.js
@@ -0,0 +1,14 @@
+/*
+
+Una consideración importante es la comida, en particular,
+la cantidad de calorías que lleva cada Elfo (su entrada de rompecabezas). 
+Los Elfos se turnan para anotar el número de Calorías que contienen las distintas comidas, 
+meriendas, raciones, etc. que han traído consigo, un elemento por línea. 
+Cada Elfo separa su propio inventario del inventario del Elfo anterior (si lo hay) con una línea en blanco. 
+
+input: kal por elfo
+cada linea corresponde a las kal de un elfo 
+linea en blanco para separar la comida anterior 
+
+*/
+
